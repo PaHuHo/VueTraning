17df1e5523e99d1fcf65138921f1381a978b1555
PaHuHo <hoang.phan804@gmail.com>
2022-11-29 15:19:54 +0700

    Lesson 5 End

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
     data(){
         return {
-            product:'Giày nam'
+            product:'Giày nam',
+            image:'./assets/img/product-1.jpg',
+            inventory:5,
+            details:['50% cotton','30% wool','20% polyesfer'],
+
+            variants:[
+                {id:2234,color:"green"},
+                {id:2235,color:"brown"}
+            ]
         }
     }
 })
